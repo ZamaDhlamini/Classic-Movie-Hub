9ece707059ab6d8079c0905fdc4cccc960328df8
Zama_Dhlamini <79308075+ZamaDhlamini@users.noreply.github.com>
2023-05-18 18:25:19 +0200

    added the providers but ran into errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { RestfulProvider } from "restful-react";
 import App, { AppProps } from 'next/app';
 import '../styles/navbar.css';
+import MovieProvider from "../providers/movies";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
