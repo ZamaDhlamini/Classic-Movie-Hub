a75eb2f54a1889768cbf1ded75234a4e40e5d2bc
Zama_Dhlamini <79308075+ZamaDhlamini@users.noreply.github.com>
2023-05-19 14:21:49 +0200

    created my login page

---
+++ pages/Login/index.tsx
@@ -0,0 +1,1 @@
+//login logic goes here
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,63 +1,47 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import { useMovie } from '../providers/movies';
+import { useEffect } from 'react';
 
 const AboutPage = () => {
-  const images = [
-    {
-      src: 'dune-part-2-poster.jpg',
-      alt: 'Image 1',
-      caption: 'Dune Part two',
-    },
-    {
-      src: 'the-flash-poster.jpg',
-      alt: 'Image 2',
-      caption: 'The Flash',
-    },
-    {
-      src: 'oppenheimer.jpg',
-      alt: 'Image 2',
-      caption: 'Oppenheimer',
-    },
-    {
-      src: 'fools-paradise.jpg',
-      alt: 'Image 2',
-      caption: 'Fools Paradise',
-    },
-    {
-      src: 'the-flash-poster.jpg',
-      alt: 'Image 2',
-      caption: 'The Flash',
-    },
-    {
-      src: 'the-flash-poster.jpg',
-      alt: 'Image 2',
-      caption: 'The Flash',
-    },
-    {
-      src: 'the-flash-poster.jpg',
-      alt: 'Image 2',
-      caption: 'The Flash',
-    },
-    // Add more images as needed
-  ];
+  const { getMovies, MovieGotten } = useMovie();
 
-  return(
-  <Layout title="About | Next.js + TypeScript Example">
-    <h1>Movie Gallery</h1>
-    <p>These are your movies</p>
-    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '16px'}} >
-    {images.map((image, index) => (
-      <div key={index}>
-        <img src={image.src} alt={image.alt} style={{width: '300px', height: '420px'}} />
-        <p>{image.caption}</p>
-    </div>
-    ))}
-    </div>
-    <p>
-      <Link href="/">Go home</Link>
-    </p>
-  </Layout>
-  )
-}
+  useEffect(() => {
+    getMovies();
+    console.log('movie is fetched');
+  }, []);
 
-export default AboutPage
+  return (
+    <Layout title="About | Next.js + TypeScript Example">
+      <h1>Movie Gallery</h1>
+      <p>These are your movies</p>
+      <div className="movie-grid">
+        {MovieGotten.map((movie) => (
+          <div key={movie.id} className="movie-item">
+            <h1>{movie.title}</h1>
+            {/* <p>Description: {movie.description}</p> */}
+            <img src={movie.picture} alt={movie.title} width={'300px'} height={'420px'} />
+          </div>
+        ))}
+      </div>
+      <p>
+        <Link href="/">Go home</Link>
+      </p>
+
+      <style jsx>{`
+        .movie-grid {
+          display: grid;
+          grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+          grid-gap: 20px;
+        }
+
+        .movie-item {
+          /* border: 1px solid #ccc; */
+          padding: 10px;
+        }
+      `}</style>
+    </Layout>
+  );
+};
+
+export default AboutPage;
