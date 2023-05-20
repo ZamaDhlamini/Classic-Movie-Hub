ad92dd6f438c4f51c907f3380ff5fc0cff642b2f
Zama_Dhlamini <79308075+ZamaDhlamini@users.noreply.github.com>
2023-05-20 14:14:46 +0200

    got the youtube url working sort of

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,40 +5,39 @@ import { useGet } from 'restful-react';
 import { useMovie } from '../providers/movies';
 
 const IndexPage = () => {
-
-  const { getMovies, MovieGotten} = useMovie();
+  const { getMovies, MovieGotten } = useMovie();
 
   useEffect(() => {
-      getMovies();
-      console.log('movie is fetched')
-    }, []);
+    getMovies();
+    console.log('movie is fetched');
+  }, []);
 
-    const extractYouTubeVideoId = (url) => {
-      if (url) {
-        const trailerId = url.replace('https://youtu.be/', '');
-        return trailerId;
-      }
-      return null;
-    };
-    
-    const trailerId = extractYouTubeVideoId(MovieGotten.videoURL);
+  const extractYouTubeVideoId = (url) => {
+    if (url) {
+      const trailerId = url.replace('https://youtu.be/', '');
+      return trailerId;
+    }
+    return null;
+  };
+
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>Welcome to Movie Buff! 👋</h1>
       <div>
-        {MovieGotten.map((movie) => (
+        {MovieGotten?.map((movie) => (
           <div key={movie.id}>
             <h1>{movie.title}</h1>
             <p>Duration: {movie.duration}</p>
-            <p>Staring: {movie.starring}</p>
+            <p>Starring: {movie.starring}</p>
             <p>Genre: {movie.genreName}</p>
             <p>Year: {movie.year}</p>
             <p>Description: {movie.description}</p>
-            <img src={movie.picture} alt={movie.title} width={'300px'} height={'420px'}/>
-            <video width="300" height="auto" controls>
-              <source src={movie.videoUrl} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
+            <img src={movie.picture} alt={movie.title} width={'300px'} height={'420px'} />
+            <iframe
+              className={movie.videoUrl}
+              src={`https://www.youtube.com/embed/${extractYouTubeVideoId(movie.videoUrl)}`}
+              allowFullScreen
+            ></iframe>
           </div>
         ))}
       </div>
